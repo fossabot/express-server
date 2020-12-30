583eed36d8efdb6d93630456a4eb4d550d8d30b6
JPSO <jpso.guerreiro@gmail.com>
2020-12-30 09:03:26 -0300

    Fix syntax

--- core.js
+++ core.js
@@ -2,7 +2,8 @@ let express = require("express");
 let app = express() //the server
 app.use(
     function(req, res, next) {
-  express.static('.');
+  express.static('E:\\');
   next()
+}
 )
 app.listen(80)
