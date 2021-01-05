d7bf0062137ca1a6293d9ba2a5c714f48bd062a7
JPSO <jpso.guerreiro@gmail.com>
2021-01-05 10:26:10 -0300

    .

--- core.js
+++ core.js
@@ -1,5 +1,19 @@
 const express = require('express')
-const app = express() // the server
+const app = express()
+app.use(function(req, res, next){
+  try{
+  if (path.isDirectory(req.path)){
+    res.write("Dir requested")
+    return;
+  }else{
+    next()
+  }
+
+}catch(e){
+  res.write(String(e))
+  return;
+}
+})// the server
 app.use(express.static('.'))
 app.listen(80, () => {
   console.log('listening')
